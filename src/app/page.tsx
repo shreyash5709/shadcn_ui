fa5d87fb352fdc7a7435665b5f6b25f362da530c
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-03-26 19:26:20 +0530

    basics use of shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       <div>Shreyash's Portfolio </div>
       <button className="bg-blue-500 hover:bg-blue-700 
       text-white font-bold py-2 px-4 rounded my-3">Contact Me</button>
+      <Button variant='chai'>Contact Me</Button>
     </main>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        chai: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3",
       },
       size: {
         default:
